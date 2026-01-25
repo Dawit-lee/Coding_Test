63ab5a551535632bdb53d6ac65faa041dd8cdfe7
Dawit-lee <edw1372@naver.com>
2026-01-26 00:25:40 +0900

    [Bronze V] Title: 검증수, Time: 172 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,19 @@
+//  검증수
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int n = sc.nextInt();
+            sum += (n*n);
+        }
+        System.out.println(sum % 10);
+    }
+}
