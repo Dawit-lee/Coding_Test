2ed2ca532a46c70b3edfdd9eee83f366525f4b2a
Dawit-lee <edw1372@naver.com>
2026-01-27 18:39:22 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 276 ms, Memory: 18352 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+
+		int n = scanner.nextInt();
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 0; j < n - i; j++) {
+				System.out.print(" ");
+				}
+				for (int k = 0; k < i; k++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+	}
