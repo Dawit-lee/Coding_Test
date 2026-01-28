a2d92cdb594fb7ebd772c12a0b29a7748c3e7392
Dawit-lee <edw1372@naver.com>
2026-01-28 20:02:02 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+//  아스키 코드
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		char ch = sc.next().charAt(0);
+
+		System.out.print((int) ch);
+	}
+}
