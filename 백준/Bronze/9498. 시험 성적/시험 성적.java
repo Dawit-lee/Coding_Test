47756589b81e52bcbb6b08caf44608a9b7626c3f
Dawit-lee <edw1372@naver.com>
2026-02-18 17:02:40 +0900

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+
+		int score = Integer.parseInt(str);
+
+		if (score >= 90 && score <= 100) {
+			System.out.println("A");
+		} else if (score < 90 && score >= 80) {
+			System.out.println("B");
+		} else if (score < 80 && score >= 70) {
+			System.out.println("C");
+		} else if (score < 70 && score >= 60) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+}
