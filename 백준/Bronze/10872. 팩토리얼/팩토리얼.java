10263981a7c039c4c131eca601748f4984b8f988
Dawit-lee <edw1372@naver.com>
2026-02-27 09:51:47 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 1;
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            sum = sum * i;
+        }
+        System.out.printf("%d", sum);
+    }
+}
