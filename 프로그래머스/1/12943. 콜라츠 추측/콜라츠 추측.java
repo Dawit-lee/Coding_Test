1fb8c930a41013629b62cee4dda1b368c998b3f2
Dawit-lee <edw1372@naver.com>
2026-03-04 14:05:36 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int num) {
+        long n = (long) num;
+        int answer = 0;
+
+        if (num == 1) return 0;
+        
+        // if (int num < 1 || int num > 8000000) return 0;
+            
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = (n * 3) + 1;
+            }
+            
+            answer++;
+                
+            if (answer >= 500) {
+                return -1;
+            }
+        }
+        return answer;
+        }
+    }
