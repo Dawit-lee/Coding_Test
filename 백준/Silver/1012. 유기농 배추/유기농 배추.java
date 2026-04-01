2eb429ea51287398b61512a4a6439396cc821a05
Dawit-lee <edw1372@naver.com>
2026-04-01 21:09:35 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,34 +1,39 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] g;
+    static int[][] grid;
+    static boolean[][] visited;
     static int N, M;
-    static int[] dx = {0,0,1,-1}, dy = {1,-1,0,0};
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
+        int T = Integer.parseInt(br.readLine());
 
-        for (int t = 0; t < T; t++) {
+        while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
-            
-            g = new int[N][M];
-            
-            for (int k = 0; k < K; k++) {
+
+            grid = new int[N][M];
+            visited = new boolean[N][M];
+
+            for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                g[y][x] = 1;
+                grid[y][x] = 1;
             }
 
             int count = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    if (g[i][j] == 1) {
+                    if (grid[i][j] == 1 && !visited[i][j]) {
                         dfs(i, j);
                         count++;
                     }
@@ -39,10 +44,15 @@ public class Main {
     }
 
     static void dfs(int x, int y) {
-        g[x][y] = 0;
+        visited[x][y] = true;
         for (int i = 0; i < 4; i++) {
-            int nx = x+dx[i], ny = y+dy[i];
-            if (nx>=0 && nx<N && ny>=0 && ny<M && g[nx][ny]==1) dfs(nx, ny);
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                if (grid[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
         }
     }
 }
